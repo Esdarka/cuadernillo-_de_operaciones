3168dfd63be88e59e816b03da8cf036ea1f40333
oscar <116449809+Esdarka@users.noreply.github.com>
2022-10-26 00:33:58 -0500

    cuadernillo
    Aquí están los 10 ejercicios del cuadernillo solicitado

---
+++ CUADERNILLO/EJERCICIO 4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace precio_agua
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double alto, largo, ancho,TOTAL;
+            double Can_m,P_m=45, P_TOTAL;
+            Console.WriteLine("CALCULAR LOS METROS CUBICOS DE UNA SISTERNA");
+            Console.WriteLine("INGRESE LA CANTIDAD DE METROS DE ALTO");
+            alto= double.Parse(Console.ReadLine());
+            Console.WriteLine("INGRESE LA CATIDAD DE METROS DE LARGO");
+            largo= double.Parse(Console.ReadLine());
+            Console.WriteLine("INGRESE LA CANTIDAD DE METROS DEL ANCHO");
+            ancho = double.Parse(Console.ReadLine());
+
+            {
+                TOTAL = (alto * largo * ancho);
+            }
+            Console.WriteLine("EL TOTAL DE METROS CUBICOS SON : "   + TOTAL + " M3 ");
+
+            Console.WriteLine("INGRESE LA CANTIDAD DE METROS CUBICO DE AGUA");
+            Can_m= double.Parse(Console.ReadLine());
+
+            {
+                P_TOTAL = (P_m * Can_m);
+            }
+
+            Console.WriteLine("LA CANTIDAD A PAGAR PARA LLENAR SU DEPOSITO DE AGUA ES DE  : "+P_TOTAL);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CUADERNILLO/EJERCICIO 6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EJERCICIO6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Realizar un programa que dado un operario se conoce su sueldo y los años de antigüedad.");
+            Console.WriteLine("Realiza un programa que lea los datos de entrada e indicar:");
+            Console.WriteLine(" Si el sueldo es igual o inferior a $10000 y su antigüedad es igual o superior a 5 años, otorgarle un aumento del 25 %, mostrar el sueldo a pagar.");
+            Console.WriteLine("Si el sueldo es inferior a $10000 pero su antigüedad es menor a 5 años, otorgarle un aumento de 15 %.");
+            Console.WriteLine("Si el sueldo es mayor o igual a 10000 mostrar el sueldo.");
+            Console.WriteLine("..............................................................................................................");
+
+            double sueldo, sueldoTot, aument;
+            int antiguedad;
+
+            Console.Write("Ingrese sueldo del empleado: ");
+            sueldo = float.Parse(Console.ReadLine());
+            Console.Write("Ingrese su antiguedad en años: ");
+            antiguedad = int.Parse(Console.ReadLine());
+            if (sueldo <= 10000 && antiguedad >= 5)
+            {
+                aument = sueldo * 0.25;
+                sueldoTot = sueldo + aument;
+                Console.Write("Sueldo a pagar:" + sueldoTot);
+            }
+            else
+            {
+                if (sueldo < 10000)
+                {
+                    aument = sueldo * 0.15;
+                    sueldoTot = sueldo + aument;
+                    Console.Write("Sueldo a pagar:" + sueldoTot);
+                }
+                else
+                {
+                    Console.Write("Sueldo a pagar:" + sueldo);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
